36b5e954b8d21ebd7c25e35c47ce0afed219384a
4L4N <digitalforce.ac@gmail.com>
2023-06-28 17:04:58 -0600

    completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+  let newstr = "";
 
+  if(times < 0) {
+    return "ERROR";
+  }
+
+  for(let i = 0; i < times; i++) {
+    newstr = newstr.concat(str);
+  }
+
+  return newstr;
 };
 
 // Do not edit below this line
