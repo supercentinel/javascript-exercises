9cae7087d5f118dcfa69491172254316045c4f53
4L4N <digitalforce.ac@gmail.com>
2023-06-29 22:23:09 -0600

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let sum = 0;
 
+  if (typeof(start) != "number" || typeof(end) != "number") {
+    return "ERROR";
+  }
+
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
+
+  if(start > end) {
+    dummy = start;
+    start = end;
+    end = dummy;
+  }
+
+  for(let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum
 };
 
 // Do not edit below this line
