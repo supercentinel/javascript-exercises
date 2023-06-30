b417f64760866ff015dbd708fbf693fc82e3d98f
4L4N <digitalforce.ac@gmail.com>
2023-06-29 23:38:26 -0600

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  c = (fahrenheit - 32)/(1.8);
+
+  if (Number.isInteger(c) == true) {
+    return c;
+  } else {
+    return Number(c.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  f = ((celsius * 1.8) + 32);
+
+  if (Number.isInteger(f) == true) {
+    return f;
+  } else {
+    return Number(f.toFixed(1));
+  }
 };
 
 // Do not edit below this line
