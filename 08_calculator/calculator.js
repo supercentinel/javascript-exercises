243d263c34ead1dbd25fc4ccbaf07f9ff36aa713
4L4N <digitalforce.ac@gmail.com>
2023-07-07 18:22:50 -0600

    completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+
+  for(let i = 0; i < a.length; i++) {
+    total += a[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(...args) {
+  let result = 1;
 
+  for(let i = 0; i < args.length; i++) {
+    result *= args[i];
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+
+  for(let i = 1; i < b; i++) {
+    result *= a;
+  }
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+
+  if(a == 1 || a == 0) {
+    return 1;
+  } else {
+    return a * factorial(a-1);
+  }
 };
 
 // Do not edit below this line
