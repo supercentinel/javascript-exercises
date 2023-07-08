4a47dd2edebfc33e344dcd1224cbd54d80bf993e
4L4N <digitalforce.ac@gmail.com>
2023-07-07 23:29:53 -0600

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+  if(n < 0) {
+    return "OOPS";
+  }
+
+  if(n == 2 || n == 1 || n == 0) {
+    return 1;
+  } else {
+    return fibonacci(n-2) + fibonacci(n-1);
+  }
 };
-
+/*
+0,1,1,2,3
+0,1,2,3,4,5
+*/
 // Do not edit below this line
 module.exports = fibonacci;
