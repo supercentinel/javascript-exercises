979d55f40e371270c6af08c17ea5aab809401df2
4L4N <digitalforce.ac@gmail.com>
2023-07-07 19:01:47 -0600

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let strArr = str.replace(/[^a-zA-Z0-9]/g, '');
+  strArr = strArr.toLowerCase()
+  strArr = strArr.trim();
 
+  let rts = "";
+  let str_len = strArr.length;
+
+  for(let i = 1; i <= str_len; i++) {
+    rts = rts.concat(strArr.charAt(str_len-i));
+  }
+
+  if(strArr == rts) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
